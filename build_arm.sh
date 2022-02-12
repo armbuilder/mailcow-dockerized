8da34916451ef77b76d3d606a66ce31ce4d371ed
armbuilder <96950243+armbuilder@users.noreply.github.com>
2022-02-12 18:26:00 +0100

    Create  build_arm.sh

---
+++ build_arm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+version=2.2022
+type=mcow
+bpath=$PWD
+mpath=/data/Dockerfiles/
+tag=vikinger84/
+build_folders=(watchdog unbound postfix phpfpm olefy netfilter dockerapi acme sogo dovecot rspamd )
+#build_folders=(watchdog unbound postfix phpfpm olefy netfilter dockerapi acme )
+
+
+for item in ${build_folders[*]}
+do
+   echo ${bpath}${mpath}${item}${file}
+   sudo docker buildx build  ${bpath}${mpath}${item} -t ${tag}${item}':'${type}'-'${version} --platform armhf --push
+done
+
+
